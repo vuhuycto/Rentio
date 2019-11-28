aead788e67cda44af4a0500557d91fcc79aeaaca
Vu Huy <vuhuycto@gmail.com>
2019-11-28 14:19:06 +0700

    Fifth commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask
 from database import database as db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:sesame@localhost:3306/rentio'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_ECHO'] = True
 app.secret_key = os.urandom(16)
 
