133fa1255bffe35b5f2379014931b6bae103f528
Vu Huy <vuhuycto@gmail.com>
2019-11-30 15:33:24 +0700

    Test

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 import os
-import datetime
 
 from flask import Flask
-from database import database as db
+from flask_restful import Api
+from database import Base, Session, engine
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-app.config['SQLALCHEMY_ECHO'] = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+# app.config['SQLALCHEMY_ECHO'] = True
 app.secret_key = os.urandom(16)
 
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return os.getcwd()
 
+
+api = Api(app)
+
+Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
     app.run()
+
--- database.py
+++ database.py
@@ -1,3 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, relationship
 
-database = SQLAlchemy()
+engine = create_engine("mysql+pymysql://root:sesame@localhost:3306/test")
+
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
+session = Session()
