63ba55ac60606f2335093deb7fdbca010bdbf761
Vu Huy <vuhuycto@gmail.com>
2019-11-30 15:41:25 +0700

    Test

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, relationship
 
-engine = create_engine(os.environ("DATABASE_URL", "sqlite:///data.db"))
+engine = create_engine(os.environ.get("DATABASE_URL", "sqlite:///data.db"))
 
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
