137d48ba0c6712ef5549ce28fed615446718eb85
Vu Huy <vuhuycto@gmail.com>
2019-12-18 12:24:21 +0700

    ssl

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(os.environ.get("DATABASE_URL", "sqlite:///data.db"))
+engine = create_engine(os.environ.get("DATABASE_URL", "sqlite:///data.db"), connect_args={'sslmode':'require'}, echo=True)
 
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
