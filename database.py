d2f2284be7fc5b423e815d5203ea9a96f51dcb55
Vu Huy <vuhuycto@gmail.com>
2019-12-19 10:37:07 +0700

    Change database

--- database.py
+++ database.py
@@ -5,8 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from multiprocessing.util import register_after_fork
 
 # engine = create_engine("postgresql+psycopg2://postgres:sesame@localhost:5432/rentio")
-# engine = create_engine(os.environ.get("DATABASE_URL", "sqlite:///data.db"))
-engine = create_engine(os.environ.get("postgres://rbllcgxtmntvhu:ad16fdf39b9cb08a798452e4bf2ab374ab15863748ab41ed6125331a1f937dd7@ec2-46-137-187-23.eu-west-1.compute.amazonaws.com:5432/dc7htujnbptmm9", "sqlite:///data.db"))
+engine = create_engine(os.environ.get("DATABASE_URL", "sqlite:///data.db"))
 register_after_fork(engine, engine.dispose)
 
 Base = declarative_base()
